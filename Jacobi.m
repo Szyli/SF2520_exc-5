a682bceae3a94b4b087bffdb014ca952242c23f7
Szyli <csabaszilard2000@gmail.com>
2023-11-28 11:57:07 +0100

    fixed function

--- Jacobi.m
+++ Jacobi.m
@@ -1,21 +1,20 @@
 function [iter_num,rel_res_size] = Jacobi(A,b,x_k,eps,K)
     %diagonal of A; in lecture notes it's named 'M'; is sparse
     M = diag(diag(A));
-    
     T = A - M;
-    
-    x = x_k + 1;
+    x_guess = x_k + 1;
     
     %iteration
-    rel_res_size = [];
+    rel_res_size = norm(b-A*x_k,2)/norm(b,2);
     iter_num = 0;
-    while norm(b - A*x_k) > eps * norm(b)   %there is no do while in matlab...
-
+    while norm(x_k - x_guess,2) > eps   %there is no do while in matlab...
+        x_guess = x_k;
         %lecture 14; page: 18/20
-        x_k = M\(T*x_k+b); %inv(D)*(T*x(:,k)+b); 
+        x_k = M\(b - T*x_k); %inv(D)*(T*x(:,k)+b); 
+
         iter_num = iter_num + 1;
-        %rel_res_size = [rel_res_size; norm(b-A*x_k)/norm(b)];
-        rel_res_size = [rel_res_size; norm(b - x_k)];
+        rel_res_size = [rel_res_size; norm(b-A*x_k,2)/norm(b,2)];
+        
         if iter_num > K
             break
         end
